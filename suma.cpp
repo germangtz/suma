f9817a43d8ce598a6b6143b16db3d60b8c3d428c
germangtz <licgerman@gmail.com>
2024-10-16 08:36:59 -0600

    Se agregaron comentarios

--- suma.cpp
+++ suma.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int main()
 {
+    // DECLARACION DE VARIABLES
     int a, b;
+    // LECTURA DE DATOS DE ENTRADA
     cin >>a >> b;
+    // IMPRESION DEL RESULTADO
     cout << a+b << endl;
 }
